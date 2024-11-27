b12f5381074f11f93bbb4d5b1fb4435bde28bf86
romilsingal23 <153853468+romilsingal23@users.noreply.github.com>
2024-11-27 20:59:57 +0530

    Update Prisma.ps1

--- Prisma.ps1
+++ Prisma.ps1
@@ -1,35 +1,35 @@
+def generate_user_data_script(image, prisma_base_url, path_to_console, token):
+    response = create_secret('prisma-token', token)
+    print("response", response)
+    if image['os_type'] == 'Windows':
+        script = f"""
 <powershell>
-# Retrieve the token using gcloud
-$bearer = (gcloud secrets versions access latest --secret=prisma-token --project=zjmqcnnb-gf42-i38m-a28a-y3gmil).Trim()
-
-# Define parameters for Invoke-WebRequest
+$bearer = (gcloud secrets versions access latest --secret=prisma-token --project={project_id})
 $parameters = @{
-    Uri     = 'https://us-east1.cloud.twistlock.com/us-1-111573393/api/v1/scripts/defender.ps1'
-    Method  = "Post"
-    Headers = @{ "Authorization" = "Bearer $bearer" }
-    OutFile = "defender.ps1"
-}
-
-# Add trust for SSL/TLS certificates (if required)
-if ($PSEdition -eq "Desktop") {
+    Uri = '{path_to_console}/api/v1/scripts/defender.ps1';
+    Method = "Get";
+    Headers = @{{ "Authorization" = "Bearer $bearer" }};
+    OutFile = "defender.ps1";
+};
+# Handle SSL certificate validation for the console endpoint
+if ($PSEdition -eq 'Desktop') {{
     Add-Type -TypeDefinition @"
-        using System.Net;
-        using System.Security.Cryptography.X509Certificates;
-        public class TrustAllCertsPolicy : ICertificatePolicy {
-            public bool CheckValidationResult(ServicePoint srvPoint, X509Certificate certificate, WebRequest request, int certificateProblem) { return true; }
-        }
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {{
+        public bool CheckValidationResult(ServicePoint srvPoint, X509Certificate certificate, WebRequest request, int certificateProblem) {{
+            return true;
+        }}
+    }}
 "@
-    [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
-} else {
-    $parameters.SkipCertificateCheck = $true
-}
-
-# Set the security protocol to TLS 1.2
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-# Make the web request to download the defender script
+    [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy;
+}} else {{
+    $parameters.SkipCertificateCheck = $true;
+}}
+# Configure TLS settings
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
+# Download and execute the Prisma Defender script
 Invoke-WebRequest @parameters
-
-# Execute the downloaded script
-.\defender.ps1 -type serverWindows -consoleCN us-east1.cloud.twistlock.com -install -u
+.\defender.ps1 -type serverWindows -consoleCN {prisma_base_url} -install -u
 </powershell>
+"""
