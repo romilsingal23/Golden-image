67f7615467fa188483e996afda7bb6759d6235e2
romilsingal23 <153853468+romilsingal23@users.noreply.github.com>
2024-11-30 14:37:59 +0530

    Update Adoption.sh

--- Adoption.sh
+++ Adoption.sh
@@ -1,15 +1,5 @@
-# Get all projects in the organization
-for project in $(gcloud projects list --filter="parent.type=organization" --format="value(projectId)"); do
-  echo "Project: $project"
-  # List all VM instances in each project and their images
-  instances=$(gcloud compute instances list --project="$project" --format="value(name,zone,disks[].source)")
-
-  for instance_info in $instances; do
-    image_url=$(echo "$instance_info" | awk -F '/' '{print $NF}')
-    
-    if [[ $image_url == *"images"* ]]; then
-      echo "Fetching labels for image: $image_url in project: $project"
-      gcloud compute images describe "$image_url" --project="$project" --format="json(labels)"
-    fi
-  done
-done
+gcloud logging read \
+    'resource.type="gce_instance" AND protoPayload.methodName="v1.compute.instances.insert"' \
+    --project=YOUR_PROJECT_ID \
+    --format="json(protoPayload.authenticationInfo.principalEmail, resource.labels.instance_id, protoPayload.request)" \
+    --limit 10
