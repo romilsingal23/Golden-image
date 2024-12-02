2d886fd9874e337e1794cab113cee1b84cd0aab8
romilsingal23 <153853468+romilsingal23@users.noreply.github.com>
2024-12-02 23:23:36 +0530

    Update adoption.tf

--- adoption.tf
+++ adoption.tf
@@ -1,67 +1,65 @@
-provider "google" {
-  project = "YOUR_PROJECT_ID"  # Replace with your project ID
-  region  = "us-central1"       # Cloud Function region
+data "archive_file" "adoption" {
+  type        = "zip"
+  source_dir  = "${path.module}/adoption"
+  output_path = "${path.module}/adoption.zip"
 }
 
-resource "google_storage_bucket" "bucket" {
-  name     = "rsingal-gcp-build-bucket"  # Replace with your GCS bucket name
-  location = "US"
+resource "google_storage_bucket_object" "adoption" {
+  name           = "adoption.zip"
+  bucket         = google_storage_bucket.gcp_build.name
+  source         = data.archive_file.adoption.output_path
+  detect_md5hash = filebase64sha256(data.archive_file.adoption.output_path)
 }
 
-resource "google_cloudfunctions2_function" "vm_image_labels_function" {
-  name        = "export-vm-image-labels"
+
+resource "google_cloudfunctions2_function" "adoption_function" {
+  name        = "${local.namespace_}adoption_report"
   description = "Cloud Function to export VM image labels"
 
-  region      = "us-central1"
-  runtime     = "python310"   # Specify runtime as Python 3.10
-  entry_point = "export_vm_image_labels"
+  location = "us-east1"
 
   build_config {
     entry_point = "export_vm_image_labels"
+    runtime     = "python310"
     source {
       storage_source {
-        bucket = google_storage_bucket.bucket.name
-        object = "vm_image_labels_code.zip"  # Replace with the location of the ZIP file
+        bucket = google_storage_bucket.gcp_build.name
+        object = google_storage_bucket_object.adoption.name
       }
     }
+    environment_variables = {
+      BUCKET_NAME = google_storage_bucket.gcp_build.name
+      PROJECT_ID  = var.project_id
+    }
+
+  }
+  service_config {
+    max_instance_count  = 3
+    min_instance_count = 1
+    available_memory    = "4Gi"
+    timeout_seconds     = 60
+    max_instance_request_concurrency = 80
+    available_cpu = "4"
+    #service_account_email        = google_service_account.adoption_sa.email
+ 
   }
 
-  service_account_email = "YOUR_SERVICE_ACCOUNT@YOUR_PROJECT_ID.iam.gserviceaccount.com"  # Replace with your service account
-
-  environment_variables = {
-    BUCKET_NAME = "rsingal-gcp-build-bucket"
-  }
 }
 
-resource "google_storage_bucket_object" "function_code" {
-  name   = "vm_image_labels_code.zip"
-  bucket = google_storage_bucket.bucket.name
-  source = "vm_image_labels_code.zip"  # Path to your zip file containing your Cloud Function code
-}
-
-resource "google_cloud_scheduler_job" "weekly_job" {
+resource "google_cloud_scheduler_job" "adoption_job" {
   name        = "weekly-export-vm-image-labels"
   description = "Job to run the export_vm_image_labels function weekly"
-  schedule    = "0 9 * * 1"  # Cron for weekly, every Monday at 9:00 AM
-  time_zone   = "America/Los_Angeles"
+  schedule    = "0 9 * * 1" # Cron for weekly, every Monday at 9:00 AM
+  time_zone   = "UTC"
 
   http_target {
-    uri        = google_cloudfunctions2_function.vm_image_labels_function.https_trigger_url
+    uri         = google_cloudfunctions2_function.adoption_function.service_config[0].uri
     http_method = "POST"
+    oidc_token {
+      service_account_email = google_service_account.scheduler_sa.email
+    }
     headers = {
       "Content-Type" = "application/json"
     }
   }
-
-  # Allow Cloud Scheduler to invoke the function
-  iam_member {
-    role   = "roles/cloudfunctions.invoker"
-    member = "serviceAccount:${google_cloud_scheduler_job.weekly_job.service_account_email}"
-  }
-}
-
-resource "google_project_iam_member" "cloud_scheduler_invoker" {
-  project = "YOUR_PROJECT_ID"  # Replace with your project ID
-  role    = "roles/cloudfunctions.invoker"
-  member  = "serviceAccount:${google_cloud_scheduler_job.weekly_job.service_account_email}"
 }
