a1998d78115a1081257a631e9a6c4a97e81972b0
romilsingal23 <153853468+romilsingal23@users.noreply.github.com>
2024-12-05 21:01:18 +0530

    Update Delete_artifact.sh

--- Delete_artifact.sh
+++ Delete_artifact.sh
@@ -3,10 +3,10 @@
 # Print status message
 echo "Fetching packages from Artifact Registry..."
 
-# Fetch the list of Docker images, split by '/' to get the last part (image name), and store in a file
+# Fetch the list of Docker images, split by 'gcf-artifacts/' to get everything after it, and store in a file
 gcloud artifacts docker images list \
   us-east1-docker.pkg.dev/zjmqcnnb-gf42-i38m-a28a-y3gmi/gcf-artifacts \
-  --format="value(IMAGE)" | awk -F'/' '{print $NF}' > package_list.txt
+  --format="value(IMAGE)" | sed 's|.*gcf-artifacts/||' > package_list.txt
 
 # Check if the file is empty
 if [[ ! -s package_list.txt ]]; then
