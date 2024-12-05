c4650b52b43e41aacae6c8c7f9f21d263b235cc8
romilsingal23 <153853468+romilsingal23@users.noreply.github.com>
2024-12-05 21:09:19 +0530

    Update Delete_artifact.sh

--- Delete_artifact.sh
+++ Delete_artifact.sh
@@ -5,7 +5,7 @@ echo "Fetching packages from Artifact Registry..."
 
 # Fetch the list of Docker images, split by 'gcf-artifacts/' to get everything after it, and store in a variable
 IMAGE_NAMES=$(gcloud artifacts docker images list \
-  us-east1-docker.pkg.dev/zjmqcnnb-gf42-i38m-a28a-y3gmi/gcf-artifacts \
+  us-east1-docker.pkg.dev/zjmqcnnb-gf42-i38m-a28a-y3gmil/gcf-artifacts \
   --format="value(IMAGE)" | sed 's|.*gcf-artifacts/||')
 
 # Check if no images were found
@@ -21,7 +21,7 @@ for IMAGE_NAME in $IMAGE_NAMES; do
     echo "Deleting package: $IMAGE_NAME"
     # Delete the package using the full path
     gcloud artifacts docker images delete \
-      "us-east1-docker.pkg.dev/zjmqcnnb-gf42-i38m-a28a-y3gmi/gcf-artifacts/$IMAGE_NAME" --quiet
+      "us-east1-docker.pkg.dev/zjmqcnnb-gf42-i38m-a28a-y3gmil/gcf-artifacts/$IMAGE_NAME" --quiet
   else
     echo "Skipping package: $IMAGE_NAME"
   fi
