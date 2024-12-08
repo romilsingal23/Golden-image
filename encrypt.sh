7376b07ec8527e0737f15f195bd00bae27ffea7a
romilsingal23 <153853468+romilsingal23@users.noreply.github.com>
2024-12-08 19:17:03 +0530

    Update encrypt.sh

--- encrypt.sh
+++ encrypt.sh
@@ -1,22 +1,48 @@
-#!/bin/bash
+same error if impersonate service account
 
-# Inputs from Packer post-processor
-IMAGE_NAME=$1
-PROJECT_ID=$2
-KMS_KEY_NAME=$3
-LOCATION="global"  # Adjust location if needed
 
-echo "Encrypting image: $IMAGE_NAME in project: $PROJECT_ID with KMS key: $KMS_KEY_NAME"
 
-# Create a new encrypted image
-gcloud compute images create "${IMAGE_NAME}-encrypted" \
-    --source-image "$IMAGE_NAME" \
-    --source-image-project "$PROJECT_ID" \
-    --kms-key "projects/$PROJECT_ID/locations/$LOCATION/keyRings/<KEY_RING_NAME>/cryptoKeys/$KMS_KEY_NAME"
+$ LATEST_IMAGE=$(gcloud compute images describe-from-family rsingal-gim-rhel-9 --format="value(name)")   
 
-if [ $? -eq 0 ]; then
-    echo "Image $IMAGE_NAME successfully encrypted to ${IMAGE_NAME}-encrypted."
-else
-    echo "Failed to encrypt the image $IMAGE_NAME."
-    exit 1
-fi
+
+
+#gcloud compute images create "${LATEST_IMAGE}-encrypted"   --source-image "$LATEST_IMAGE"   --kms-key projects/zjmqcnnb-gf42-i38m-a28a-y3gmil/locations/us-east1/keyRings/rsingal-key-ring/cryptoKeys/rsingal_key --impersonate-service-account=rsingal-cloud-build-sa@zjmqcnnb-gf42-i38m-a28a-y3gmil.iam.gserviceaccount.com
+
+gal-cloud-build-sa@zjmqcnnb-gf42-i38m-a28a-y3gmil.iam.gserviceaccount.com;58456cf6-01d0-418e-a1bf-27fd831f826eWARNING: This command is using service account impersonation. All API calls will be executed as [rsingal-c
+
+loud-build-sa@zjmqcnnb-gf42-i38m-a28a-y3gmil.iam.gserviceaccount.com].
+
+WARNING: This command is using service account impersonation. All API calls will be executed as [rsingal-cloud-build-sa@zjmqcnnb-gf42-i38m-a28a-y3gmil.iam.gserviceaccount.com].
+
+ERROR: (gcloud.compute.images.create) Could not fetch resource:
+
+ - Cloud KMS error when using key projects/zjmqcnnb-gf42-i38m-a28a-y3gmil/locations/us-east1/keyRings/rsingal-key-ring/cryptoKeys/rsingal_key: Permission 'cloudkms.cryptoKeyVersions.useToEncrypt' denied on resource 'projects/zjmqcnnb-gf42-i38m-a28a-y3gmil/locations/us-east1/keyRings/rsingal-key-ring/cryptoKeys/rsingal_key' (or it may not exist).
+
+
+
+# gcloud kms keys list --location us-east1 --keyring rsingal-key-ring
+
+NAME
+
+  PURPOSE     ALGORITHM          PROTECTION_LEVEL LABELS              PRIMARY_ID PRIMARY_STATE
+
+projects/zjmqcnnb-gf42-i38m-a28a-y3gmil/locations/us-east1/keyRings/rsingal-key-ring/cryptoKeys/rsingal_key ENCRYPT_DECRYPT GOOGLE_SYMMETRIC_ENCRYPTION SOFTWARE     goog-terraform-provisioned=true 1   
+
+   ENABLED
+
+
+
+$ gcloud kms keys get-iam-policy rsingal_key --keyring=rsingal-key-ring --location=us-east1
+
+bindings:
+
+- members:
+
+ - serviceAccount:rsingal-cloud-build-sa@zjmqcnnb-gf42-i38m-a28a-y3gmil.iam.gserviceaccount.com
+
+ role: roles/cloudkms.cryptoKeyEncrypterDecrypter
+
+etag: BwYogNGhVxk=
+
+version: 1
+
