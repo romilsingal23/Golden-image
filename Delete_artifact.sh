7e0680bff401427c40793285006829a1542ce8ab
romilsingal23 <153853468+romilsingal23@users.noreply.github.com>
2024-12-19 22:41:47 +0530

    Update Delete_artifact.sh

--- Delete_artifact.sh
+++ Delete_artifact.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+PROJECT_ID="$1" # Input from null resource in terraform code
+
 # Print status message
 echo "Fetching Docker images from Artifact Registry..."
 
 # Fetch the list of Docker images, split by 'gcf-artifacts/' to get everything after it, and store in a variable
 IMAGE_LIST=$(gcloud artifacts docker images list \
-  us-east1-docker.pkg.dev/zjmqcnnb-gf42-i38m-a28a-y3gmil/gcf-artifacts \
+  us-east1-docker.pkg.dev/$PROJECT_ID/gcf-artifacts \
   --format="value(IMAGE)" | sed 's|.*gcf-artifacts/||' | sort | uniq)
 
 # Check if no images were found
@@ -21,7 +23,7 @@ for IMAGE_NAME in $IMAGE_LIST; do
     echo "Deleting package: $IMAGE_NAME"
     # Delete the package using the full path
     gcloud artifacts docker images delete \
-      "us-east1-docker.pkg.dev/zjmqcnnb-gf42-i38m-a28a-y3gmil/gcf-artifacts/$IMAGE_NAME" --quiet
+      "us-east1-docker.pkg.dev/$PROJECT_ID/gcf-artifacts/$IMAGE_NAME" --quiet
   else
     echo "Skipping package: $IMAGE_NAME"
   fi
