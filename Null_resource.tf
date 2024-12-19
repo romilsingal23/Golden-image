ced8715ce4d2d34aef5e8b88e5e27f305025dba8
romilsingal23 <153853468+romilsingal23@users.noreply.github.com>
2024-12-19 22:44:25 +0530

    Delete Null_resource.tf

--- Null_resource.tf
+++
@@ -1,9 +0,0 @@
-resource "null_resource" "delete_packages" {
-  provisioner "local-exec" {
-    command = "bash ${path.module}/delete_packages.sh ${var.project_id} us-east1 gcf-artifacts"
-  }
-
-  triggers = {
-    timestamp = timestamp() # Ensure it runs every time
-  }
-}
