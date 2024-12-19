f96d515ced0bd0e288e9b8f788b7d292c46fbb70
romilsingal23 <153853468+romilsingal23@users.noreply.github.com>
2024-12-19 22:38:19 +0530

    Update gcp_network.tf

--- gcp_network.tf
+++ gcp_network.tf
@@ -1,35 +1,54 @@
 # VPC Network Creation
-resource "google_compute_network" "build_network" {
-  count      = local.is_local ? 0 : 1
-  name       = local.build_name
+resource "google_compute_network" "gcp_build_network" {
+  name                    = "${local.namespace-}gcp-build-network"
   auto_create_subnetworks = false
-  project    = var.project_id
+  project                 = var.project_id
 }
 
 # Subnet Creation
-resource "google_compute_subnetwork" "build_network" {
-  for_each   = {
-    "${local.build_name}" : local.subnet_range
-  }
-  name       = each.key
-  ip_cidr_range = each.value
-  region     = var.region
-  network    = google_compute_network.build_network[0].name
-  project    = var.project_id
+resource "google_compute_subnetwork" "gcp_build_subnet" {
+  name          = "${local.namespace-}gcp-build-subnet"
+  ip_cidr_range = var.subnet_cidr_range
+  region        = var.region
+  network       = google_compute_network.gcp_build_network.name
+  project       = var.project_id
 }
 
-# Firewall (equivalent to Network Security Group)
-resource "google_compute_firewall" "build_network" {
-  name       = local.build_name
-  network    = google_compute_network.build_network[0].name
-  project    = var.project_id
+# Firewall 
+resource "google_compute_firewall" "gcp_build_firewall" {
+  name    = "${local.namespace-}packer-firewall"
+  network = google_compute_network.gcp_build_network.name
+  project = var.project_id
 
-  # Example firewall rule allowing SSH; adjust to your rules
   allow {
     protocol = "tcp"
-    ports    = ["22"]
+    ports    = ["22", "5986"]
   }
-  
+
   # Use tags for more specific rules as per your needs
-  target_tags = ["ssh-allowed"]
+  source_ranges = var.source_ranges
+  target_tags   = var.target_tags
+}
+
+# Cloud Router
+resource "google_compute_router" "gcp_build_router" {
+  name    = "${local.namespace-}gcp-build-router"
+  network = google_compute_network.gcp_build_network.name
+  region  = var.region
+  project = var.project_id
+}
+
+# Cloud NAT
+resource "google_compute_router_nat" "gcp_build_nat" {
+  name                               = "${local.namespace-}gcp-build-nat"
+  router                             = google_compute_router.gcp_build_router.name
+  region                             = var.region
+  project                            = var.project_id
+  nat_ip_allocate_option             = "AUTO_ONLY" # Automatically assign external IPs
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ALL"
+  }
 }
