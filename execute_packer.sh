ef423aa6de4ce4e6ef9c7be70c071e3da607eb3e
romilsingal23 <153853468+romilsingal23@users.noreply.github.com>
2024-12-19 22:19:03 +0530

    Update execute_packer.sh

--- execute_packer.sh
+++ execute_packer.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# Define image families
-IMAGE_FAMILIES=("rhel-family" "ubuntu-family" "windows-family")
-
 # Update package lists and install Python3, wget, unzip
 echo "Updating package lists and installing required packages..."
 apt-get update -y
@@ -28,30 +25,21 @@ unzip ansible.zip -d ./ansible
 
 # Print environment variables for debugging
 echo "OS_TYPE: $OS_TYPE"
+echo "SOURCE_IMAGE_FAMILY: $SOURCE_IMAGE_FAMILY"
 echo "IMAGE_FAMILY: $IMAGE_FAMILY"
-echo "IMAGE_TYPE: $IMAGE_TYPE"
-echo "IMAGE_PROJECT: $IMAGE_PROJECT"
+echo "GIM_FAMILY: $GIM_FAMILY"
+echo "SOURCE_IMAGE_PROJECT: $SOURCE_IMAGE_PROJECT"
 echo "OS_ARCH: $OS_ARCH"
 echo "DATE_CREATED: $DATE_CREATED"
 echo "NETWORK: $NETWORK"
 echo "SUBNET: $SUBNET"
+echo "CODEBUILD_BUCKET: $CODEBUILD_BUCKET"
+echo "SERVICE_ACCOUNT: $service_account_id"
+echo "KMS_KEY: $kms_key"
+echo "TOPIC_NAME: $TOPIC_NAME"
 
-# Loop through each image family and remove the "latest" label from all images
-for FAMILY in "${IMAGE_FAMILIES[@]}"; do
-    # List all images in the current family
-    IMAGE_NAMES=$(gcloud compute images list \
-        --filter="family=$FAMILY" \
-        --format="value(name)")
-
-    # Loop through each image in the family and remove the "latest" label
-    for IMAGE in $IMAGE_NAMES; do
-        echo "Removing 'latest' label from image: $IMAGE"
-        gcloud compute images update "$IMAGE" --remove-labels=latest --quiet
-    done
-done
-
-# Run Packer build
-echo "Running Packer Build..."
+# Copy snow agents from bucket to /tmp to be used by packer
+gsutil cp gs://$CODEBUILD_BUCKET/UHG_Cloud* /tmp
 
 # Determine which Packer file to use based on OS type and source image family
 if [[ "$OS_TYPE" == "Windows" ]]; then
@@ -60,7 +48,17 @@ else
    PACKER_FILE="gcp.pkr.hcl"
 fi
 
-gsutil cp gs://gcp-build-bucket/UHG_Cloud* /tmp
+# # List all images in the current family
+IMAGE_NAMES=$(gcloud compute images list --filter="family=$GIM_FAMILY" --format="value(name)")
+
+# Loop through each image in the family and remove the "latest" label
+for IMAGE in $IMAGE_NAMES; do
+    echo "Removing 'latest' label from image: $IMAGE"
+    gcloud compute images update "$IMAGE" --remove-labels=latest --quiet
+done
+
+# Run Packer build
+echo "Running Packer Build..."
 
 # Initialize and build the chosen Packer file
 /usr/local/bin/packer init "$PACKER_FILE"
@@ -69,6 +67,9 @@ gsutil cp gs://gcp-build-bucket/UHG_Cloud* /tmp
 # Sleep for 30 seconds to ensure resources are ready
 sleep 30  
 
-# Run metadata storage script
+LATEST_IMAGE=$(gcloud compute images describe-from-family $GIM_FAMILY --format="value(name)")
+gcloud compute images update "$LATEST_IMAGE" --update-labels latest=true --quiet
+
+pip install -r requirements.txt --break-system-packages
 echo "Running store_metadata.py"
 python3 store_metadata.py
